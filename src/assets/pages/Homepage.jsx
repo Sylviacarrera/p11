ab70f9e1b7bfd4a3e63ce78478c0daa7e9dc8838
Sylviacarrera <129325236+Sylviacarrera@users.noreply.github.com>
2024-04-29 23:56:08 +0200

    presentationfinal

--- src/assets/pages/Homepage.jsx
+++ src/assets/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from '../components/Banner';
-import CardsMain from '../components/cards-main';
+import CardsMain from '../components/Cards-main';
 import homeBackgroundImage from '../img/imgbackground.png'; // Importez votre image de fond
 
 const Homepage = () => {
