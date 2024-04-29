736c3884e6436943a143ff2189871c16c5091dd8
Sylviacarrera <129325236+Sylviacarrera@users.noreply.github.com>
2024-04-29 22:54:12 +0200

    componentpresentation

--- src/assets/pages/Logements.jsx
+++ src/assets/pages/Logements.jsx
@@ -2,34 +2,25 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../data/logements.json';
 import Carousel from '../components/Carousel';
-import '../style/Logements.scss';
+import LogementDetails from '../components/Presentation'; // Assurez-vous que le chemin d'accès est correct
 
 const Logements = () => {
   const { id } = useParams();
   const logement = data.find((item) => item.id === id);
 
-
-  // Création d'une étoile de classement pour le rating
-  const ratingStars = Array.from({ length: 5 }, (_, index) => (
-    index < logement.rating ? '★' : '☆'
-  )).join(' ');
-
   return (
     <div className="logement-wrapper"> {/* Conteneur avec marges */}
       <Carousel pictures={logement.pictures} />
-      <div className="logement-content"> {/* Nouvelle div pour le contenu */}
-        <h1 className="logement-title">{logement.title}</h1>
-        <div className="logement-location">{logement.location}</div>
-        <div className="logement-tags">
-          {logement.tags.map((tag, index) => <span key={index} className="tag">{tag}</span>)}
-        </div>
-        <div className="logement-host">
-          <span>{logement.host.name}</span>
-          <img src={logement.host.picture} alt={`Hôte : ${logement.host.name}`} />
-        </div>
-        <div className="logement-rating">{ratingStars}</div>
-        {/* D'autres détails du logement peuvent être ajoutés ici */}
-      </div>
+      {/* Utilisation du composant LogementDetails pour afficher les détails */}
+      <LogementDetails
+        title={logement.title}
+        location={logement.location}
+        tags={logement.tags}
+        host={logement.host}
+        rating={logement.rating}
+      />
+      {/* D'autres détails du logement peuvent être ajoutés ici */}
+      {/* Footer, etc., peuvent également être ajoutés ici si nécessaire */}
     </div>
   );
 };
--- src/assets/pages/Notfoundpage.jsx
+++ src/assets/pages/Notfoundpage.jsx
@@ -4,7 +4,7 @@ import '../style/Notfoundpage.scss'
 const NotFoundPage = () => {
   return (
     <div>
-      <h1>404</h1>
+      <h1 className="not-found-title">404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <p className="link"><a href="/">Retourner sur la page d'accueil</a></p>
       {/* Vous pouvez personnaliser davantage cette page d'erreur selon vos besoins */}
